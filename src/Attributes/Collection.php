4fe577be593dbf9220d80cc78ecd8b4c4f30c413
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2023-10-06 02:25:38 +0330

    feat: allow passing 'stringable' objects

--- src/Attributes/Collection.php
+++ src/Attributes/Collection.php
@@ -3,15 +3,41 @@
 namespace MohammadAlavi\LaravelOpenApi\Attributes;
 
 use Attribute;
+use Stringable;
 
 #[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD)]
 class Collection
 {
-    /** @var string|array<string> */
-    public array|string $name;
+    /** @var string|string[] */
+    public string|array $name;
 
     public function __construct(string|array $name = 'default')
     {
-        $this->name = $name;
+        $this->name = $this->prepareCollection($name);
+    }
+
+    private function prepareCollection(string|array $name): array
+    {
+        if (is_string($name)) {
+            return [$this->getString($name)];
+        }
+
+        return array_map(function (string $item): string {
+            return $this->getString($item);
+        }, $name);
+    }
+
+    private function getString(string $name): string
+    {
+        if ($this->isStringable($name)) {
+            return (string) (new $name());
+        }
+
+        return $name;
+    }
+
+    private function isStringable(string $name): bool
+    {
+        return class_exists($name) && is_subclass_of($name, Stringable::class, true);
     }
 }
