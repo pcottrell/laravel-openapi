ecfbe476a8353495ea5b611988b5f11fa0a19a61
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-09-13 12:59:37 +0330

    Update SecuritySchemeTest.php

--- tests/oooas/Unit/Objects/SecuritySchemeTest.php
+++ tests/oooas/Unit/Objects/SecuritySchemeTest.php
@@ -2,46 +2,61 @@
 
 namespace Tests\oooas\Unit\Objects;
 
-use MohammadAlavi\ObjectOrientedOAS\Objects\Components;
 use MohammadAlavi\ObjectOrientedOAS\Objects\OAuthFlow;
 use MohammadAlavi\ObjectOrientedOAS\Objects\SecurityScheme;
-use PHPUnit\Framework\Attributes\CoversClass;
-use Tests\UnitTestCase;
 
-#[CoversClass(SecurityScheme::class)]
-class SecuritySchemeTest extends UnitTestCase
-{
-    public function testCreateWithAllParametersWorks(): void
-    {
+describe('SecurityScheme', function (): void {
+    it('can be created with no parameters', function (): void {
+        $securityScheme = SecurityScheme::create();
+
+        expect($securityScheme->toArray())->toBeEmpty();
+    });
+
+    it('can be created with all parameters', function (): void {
         $oauthFlow = OAuthFlow::create()
             ->flow(OAuthFlow::FLOW_CLIENT_CREDENTIALS);
 
-        $securityScheme = SecurityScheme::create('OAuth2')
+        $securityScheme = SecurityScheme::create()
+            ->name('OAuth2')
             ->type(SecurityScheme::TYPE_OAUTH2)
             ->description('Standard auth')
             ->in(SecurityScheme::IN_HEADER)
             ->scheme('basic')
             ->bearerFormat('JWT')
             ->flows($oauthFlow)
-            ->openIdConnectUrl('https://goldspecdigital.com');
+            ->openIdConnectUrl('https://example.com');
 
-        $components = Components::create()
-            ->securitySchemes($securityScheme);
-
-        $this->assertSame([
-            'securitySchemes' => [
-                'OAuth2' => [
-                    'type' => 'oauth2',
-                    'description' => 'Standard auth',
-                    'in' => 'header',
-                    'scheme' => 'basic',
-                    'bearerFormat' => 'JWT',
-                    'flows' => [
-                        'clientCredentials' => [],
-                    ],
-                    'openIdConnectUrl' => 'https://goldspecdigital.com',
-                ],
+        expect($securityScheme->toArray())->toEqual([
+            'type' => 'oauth2',
+            'description' => 'Standard auth',
+            'in' => 'header',
+            'scheme' => 'basic',
+            'bearerFormat' => 'JWT',
+            'flows' => [
+                'clientCredentials' => [],
             ],
-        ], $components->toArray());
-    }
-}
+            'openIdConnectUrl' => 'https://example.com',
+        ]);
+    });
+
+    it('can create an OAuth2 security scheme', function (): void {
+        $securityScheme = SecurityScheme::oauth2();
+
+        expect($securityScheme->type)->toBe(SecurityScheme::TYPE_OAUTH2);
+    });
+
+    it('can be created with all combinations', function (string $type, string $expectedType): void {
+        $securityScheme = SecurityScheme::create()->type($type);
+
+        expect($securityScheme->type)->toBe($expectedType);
+    })->with([
+        'apiKey' => ['apiKey', SecurityScheme::TYPE_API_KEY],
+        'http' => ['http', SecurityScheme::TYPE_HTTP],
+        'oauth2' => ['oauth2', SecurityScheme::TYPE_OAUTH2],
+        'openIdConnect' => ['openIdConnect', SecurityScheme::TYPE_OPEN_ID_CONNECT],
+    ])->with([
+        'query' => ['query', SecurityScheme::IN_QUERY],
+        'header' => ['header', SecurityScheme::IN_HEADER],
+        'cookie' => ['cookie', SecurityScheme::IN_COOKIE],
+    ]);
+})->covers(SecurityScheme::class);
