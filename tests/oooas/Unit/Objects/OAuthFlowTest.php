147a7cc889c967f824b95f61e9b85e7dd9553718
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-09-13 13:54:04 +0330

    Update OAuthFlowTest.php

--- tests/oooas/Unit/Objects/OAuthFlowTest.php
+++ tests/oooas/Unit/Objects/OAuthFlowTest.php
@@ -2,37 +2,59 @@
 
 namespace Tests\oooas\Unit\Objects;
 
+use MohammadAlavi\ObjectOrientedOAS\Exceptions\InvalidArgumentException;
 use MohammadAlavi\ObjectOrientedOAS\Objects\OAuthFlow;
-use MohammadAlavi\ObjectOrientedOAS\Objects\SecurityScheme;
-use PHPUnit\Framework\Attributes\CoversClass;
-use Tests\UnitTestCase;
 
-#[CoversClass(OAuthFlow::class)]
-class OAuthFlowTest extends UnitTestCase
-{
-    public function testCreateWithAllParametersWorks(): void
-    {
+describe('OAuthFlow', function (): void {
+    it('can be created with no parameters', function (): void {
+        $oauthFlow = OAuthFlow::create();
+
+        expect($oauthFlow->toArray())->toBeEmpty();
+    });
+
+    it('can be created with all parameters', function (array $scopes): void {
         $oauthFlow = OAuthFlow::create()
             ->flow(OAuthFlow::FLOW_AUTHORIZATION_CODE)
             ->authorizationUrl('https://api.example.com/oauth/authorization')
             ->tokenUrl('https://api.example.com/oauth/token')
             ->refreshUrl('https://api.example.com/oauth/token')
-            ->scopes(['read:user' => 'Read the user profile']);
+            ->scopes($scopes);
 
-        $securityScheme = SecurityScheme::create()
-            ->flows($oauthFlow);
+        expect($oauthFlow->toArray())->toEqual([
+            'authorizationUrl' => 'https://api.example.com/oauth/authorization',
+            'tokenUrl' => 'https://api.example.com/oauth/token',
+            'refreshUrl' => 'https://api.example.com/oauth/token',
+            'scopes' => $scopes,
+        ]);
+    })->with([
+        'with scopes' => [['read:user' => 'Read the user profile']],
+        'explicit no scope' => [[]],
+    ]);
 
-        $this->assertSame([
-            'flows' => [
-                'authorizationCode' => [
-                    'authorizationUrl' => 'https://api.example.com/oauth/authorization',
-                    'tokenUrl' => 'https://api.example.com/oauth/token',
-                    'refreshUrl' => 'https://api.example.com/oauth/token',
-                    'scopes' => [
-                        'read:user' => 'Read the user profile',
-                    ],
-                ],
-            ],
-        ], $securityScheme->toArray());
-    }
-}
+    it('can be created with no scope', function (): void {
+        $oauthFlow = OAuthFlow::create()->scopes(null);
+
+        expect($oauthFlow->toArray())->toBeEmpty();
+    });
+
+    it('throws an exception when scopes is not an [string => string] array', function (array $scopes): void {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Each scope must have a string key and a string value.');
+
+        OAuthFlow::create()->scopes($scopes);
+    })->with([
+        'no string key' => [[1 => 'read:user']],
+        'no string value' => [['read:user' => 1]],
+    ]);
+
+    it('can be created with all combinations', function (string $flow, string $expectedFlow): void {
+        $oauthFlow = OAuthFlow::create()->flow($flow);
+
+        expect($oauthFlow->flow)->toBe($expectedFlow);
+    })->with([
+        'implicit' => ['implicit', OAuthFlow::FLOW_IMPLICIT],
+        'password' => ['password', OAuthFlow::FLOW_PASSWORD],
+        'clientCredentials' => ['clientCredentials', OAuthFlow::FLOW_CLIENT_CREDENTIALS],
+        'authorizationCode' => ['authorizationCode', OAuthFlow::FLOW_AUTHORIZATION_CODE],
+    ]);
+})->covers(OAuthFlow::class);
