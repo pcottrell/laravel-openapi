08cdee9ebe243a019e89eec8461225e129011b13
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-09-21 22:24:26 +0330

    Create CollectionLocatorTest.php

---
+++ tests/Unit/Collectors/CollectionLocatorTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use MohammadAlavi\LaravelOpenApi\Attributes\Collection;
+use MohammadAlavi\LaravelOpenApi\Collectors\CollectionLocator;
+use Pest\Expectation;
+use Tests\Doubles\Stubs\Collectors\Components\PathMiddlewareStub;
+
+describe('CollectionLocator', function (): void {
+    it('can collect specific collections', function (): void {
+        $locator = new CollectionLocator([
+            __DIR__ . '/../../Doubles/Stubs/Collectors/Components',
+        ]);
+
+        $result = $locator->find('test');
+
+        expect($result)->toHaveCount(10)
+            ->and($result)->each(fn (Expectation $item) => $item->toUse(Collection::class));
+    });
+
+    it('collects default collection if no collection passed', function (): void {
+        $locator = new CollectionLocator([
+            __DIR__ . '/../../Doubles/Stubs/Collectors/Components',
+        ]);
+
+        $result = $locator->find();
+
+        expect($result)->toHaveCount(11)
+            ->and($result)
+            ->each(
+                fn (Expectation $item) => $item
+                ->unless(
+                    fn () => str_contains($item->value, 'Implicit') || $item->value === PathMiddlewareStub::class,
+                    fn (Expectation $item) => $item->toUse(Collection::class),
+                ),
+            );
+    });
+})->covers(CollectionLocator::class);
