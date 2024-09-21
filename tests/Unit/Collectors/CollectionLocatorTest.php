f745ed4aa859b73b9d81c0f08907263fee107675
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-09-21 22:55:23 +0330

    Update CollectionLocatorTest.php

--- tests/Unit/Collectors/CollectionLocatorTest.php
+++ tests/Unit/Collectors/CollectionLocatorTest.php
@@ -29,7 +29,8 @@ describe('CollectionLocator', function (): void {
             ->each(
                 fn (Expectation $item) => $item
                 ->unless(
-                    fn () => str_contains($item->value, 'Implicit') || $item->value === PathMiddlewareStub::class,
+                    static fn () => str_contains($item->value, 'Implicit')
+                        || PathMiddlewareStub::class === $item->value,
                     fn (Expectation $item) => $item->toUse(Collection::class),
                 ),
             );
