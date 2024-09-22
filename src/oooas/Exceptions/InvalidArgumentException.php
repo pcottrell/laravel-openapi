88e4a864c2d201f9f17a5b2aa046100e95b37545
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-09-22 15:10:05 +0330

    Update InvalidArgumentException.php

--- src/oooas/Exceptions/InvalidArgumentException.php
+++ src/oooas/Exceptions/InvalidArgumentException.php
@@ -2,6 +2,9 @@
 
 namespace MohammadAlavi\ObjectOrientedOAS\Exceptions;
 
+// TODO: does it make sense to use the internal PHP exception InvalidArgumentException?
+//  Or does it count as a domain exception?
+//  Even though their name and purpose are the same?
 class InvalidArgumentException extends \Exception
 {
 }
