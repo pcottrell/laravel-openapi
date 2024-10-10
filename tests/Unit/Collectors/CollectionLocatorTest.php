9691e40bc792c558c77aa6b8625e436ac5b6ef1f
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-10-10 19:00:10 +0330

    Update CollectionLocatorTest.php

--- tests/Unit/Collectors/CollectionLocatorTest.php
+++ tests/Unit/Collectors/CollectionLocatorTest.php
@@ -1,20 +1,20 @@
 <?php
 
 use MohammadAlavi\LaravelOpenApi\Attributes\Collection;
-use MohammadAlavi\LaravelOpenApi\Services\CollectionLocator;
+use MohammadAlavi\LaravelOpenApi\Services\ComponentCollector;
 use Pest\Expectation;
 
 describe('CollectionLocator', function (): void {
     it('can collect specific collections', function (): void {
-        $locator = new CollectionLocator([
+        $locator = new ComponentCollector([
             __DIR__ . '/../../Doubles/Stubs/Collectors/Components',
         ]);
 
-        $result = $locator->find('test');
+        $result = $locator->collect('test');
 
         expect($result)->toHaveCount(10)
             ->and($result)->each(
                 fn (Expectation $expectation) => $expectation->toUse(Collection::class),
             );
     });
-})->covers(CollectionLocator::class);
+})->covers(ComponentCollector::class);
