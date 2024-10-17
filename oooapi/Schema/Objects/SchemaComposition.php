1c4da540e8ed3f2c192ab7c321397f4384593c36
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-10-17 21:01:18 +0330

    Update SchemaComposition.php

--- oooapi/Schema/Objects/SchemaComposition.php
+++ oooapi/Schema/Objects/SchemaComposition.php
@@ -8,6 +8,12 @@ use MohammadAlavi\ObjectOrientedOpenAPI\Contracts\Interface\SimpleCreator;
 use MohammadAlavi\ObjectOrientedOpenAPI\Schema\ExtensibleObject;
 use MohammadAlavi\ObjectOrientedOpenAPI\Utilities\Arr;
 
+// TODO: I believe SchemaComposition are not Schemas and should not extend SchemaContract
+// They combine schemas together tho.
+// So they hold a list of schemas.
+// https://json-schema.org/understanding-json-schema/reference/object
+// https://json-schema.org/understanding-json-schema/reference/combining
+// I also believe we shouldn't be allowed to be added to Schema "properties" and "items" as they are not schemas.
 abstract class SchemaComposition extends ExtensibleObject implements SchemaContract, SimpleCreator
 {
     /** @var SchemaContract|ReusableSchemaFactory[]|null */
