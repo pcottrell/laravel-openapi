8a958fae88acb3e89226bc5ff94aceaff958883b
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-10-25 18:12:51 +0330

    Update SchemaFactory.php

--- src/Contracts/Interface/Factories/Components/SchemaFactory.php
+++ src/Contracts/Interface/Factories/Components/SchemaFactory.php
@@ -3,9 +3,9 @@
 namespace MohammadAlavi\LaravelOpenApi\Contracts\Interface\Factories\Components;
 
 use MohammadAlavi\LaravelOpenApi\Contracts\Interface\Factories\ComponentFactory;
-use MohammadAlavi\ObjectOrientedOpenAPI\Contracts\Interface\SchemaContract;
+use MohammadAlavi\ObjectOrientedOpenAPI\Contracts\Interface\JsonSchema;
 
 interface SchemaFactory extends ComponentFactory
 {
-    public function build(): SchemaContract;
+    public function build(): JsonSchema;
 }
